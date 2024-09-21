e415b5c0b389009719bbb4e862c7fe5b6df13ca6
albert.deharo <albert.deharo@basetis.com>
2024-09-21 19:24:34 +0200

    [add] - simple parallax example

---
+++ src/assets/components/Parallax/Parallax.jsx
@@ -0,0 +1,14 @@
+import './Parallax.css';
+
+const Parallax = () => {
+  return (
+    <div className="parallax">
+      <div className="parallax-content">
+        <h2>¡Descubre el Poder de ACE!</h2>
+        <p>Reforzar el agarre nunca fue tan fácil. Experimenta la diferencia con nuestro spray.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Parallax;
--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -4,12 +4,14 @@ import Gallery from '../components/Gallery/Gallery';
 import HowItWorks from '../components/HowItWorks/HowItWorks';
 import Testimonials from '../components/Testimonials/Testimonials';
 import FAQ from '../components/FAQ/FAQ';
+import Parallax from '../components/Parallax/Parallax';
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <Features />
+      <Parallax />
       <HowItWorks />
       <Testimonials />
       <Gallery />
