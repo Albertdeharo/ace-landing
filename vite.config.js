4fca11fa5523e60d7003ff65dcf5d9a4ec7f0347
albert.deharo <albert.deharo@basetis.com>
2024-09-22 18:13:58 +0200

    [add] plugin gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/ace-landing/', // Asegúrate de reemplazar 'ace-landing' con el nombre exacto de tu repositorio
+});
