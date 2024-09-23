2a452c6a29d2245300a4f444f6ea1cccddd0ea81
albert.deharo <albert.deharo@basetis.com>
2024-09-23 11:28:26 +0200

    [add] - react-parallax library

--- src/assets/components/Parallax/Parallax.jsx
+++ src/assets/components/Parallax/Parallax.jsx
@@ -1,14 +1,19 @@
+import { Parallax } from 'react-parallax';
 import './Parallax.css';
 
-const Parallax = () => {
+import parallaxImg from './../../images/ace_spray2.jpg';
+
+const ParallaxSection = () => {
   return (
-    <div className="parallax">
-      <div className="parallax-content">
-        <h2>¡Descubre el Poder de ACE!</h2>
-        <p>Reforzar el agarre nunca fue tan fácil. Experimenta la diferencia con nuestro spray.</p>
+    <Parallax bgImage={parallaxImg} strength={300}>
+      <div style={{ height: 300 }}>
+        <div className="parallax-content">
+          <h2>¡Descubre el Poder de ACE!</h2>
+          <p>Reforzar el agarre nunca fue tan fácil. Experimenta la diferencia con nuestro spray.</p>
+        </div>
       </div>
-    </div>
+    </Parallax>
   );
 };
 
-export default Parallax;
+export default ParallaxSection;
