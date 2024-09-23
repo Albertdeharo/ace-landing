9fee28470d32ffe381842eedff98f25dbfba2eed
albert.deharo <albert.deharo@basetis.com>
2024-09-23 11:21:13 +0200

    [add] - active links navbar

--- src/assets/components/Navbar/Navbar.jsx
+++ src/assets/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { useTranslation } from './../../../TranslationContext'; // Cambia la ruta si es necesario
 import './Navbar.css';
 
@@ -53,9 +53,34 @@ const Navbar = () => {
         <span className="bar"></span>
       </div>
       <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
-        <li><Link to="/" onClick={toggleMenu}>{translate('home')}</Link></li>
-        <li><Link to="/about" onClick={toggleMenu}>{translate('about')}</Link></li>
-        <li><Link to="/contact" onClick={toggleMenu}>{translate('contact')}</Link></li>
+        {/* Usamos NavLink para que se aplique una clase al link activo */}
+        <li>
+          <NavLink 
+            exact to="/" 
+            activeClassName="active" 
+            onClick={toggleMenu}
+          >
+            {translate('home')}
+          </NavLink>
+        </li>
+        <li>
+          <NavLink 
+            to="/about" 
+            activeClassName="active" 
+            onClick={toggleMenu}
+          >
+            {translate('about')}
+          </NavLink>
+        </li>
+        <li>
+          <NavLink 
+            to="/contact" 
+            activeClassName="active" 
+            onClick={toggleMenu}
+          >
+            {translate('contact')}
+          </NavLink>
+        </li>
         <li className="dropdown">
           <button onClick={toggleDropdown} className="dropdown-toggle">
             {translate('language')}
@@ -64,20 +89,17 @@ const Navbar = () => {
             <ul className="dropdown-menu">
               <li>
                 <button onClick={() => changeLanguage('cat')}>
-                  <img src={flagCat} alt="Catalan flag" className="flag-icon" />
-                  {translate('language1')}
+                  <img src={flagCat} alt="Catalan flag" className="flag-icon" /> {translate('language1')}
                 </button>
               </li>
               <li>
                 <button onClick={() => changeLanguage('es')}>
-                  <img src={flagEs} alt="Spanish flag" className="flag-icon" />
-                  {translate('language2')}
+                  <img src={flagEs} alt="Spanish flag" className="flag-icon" /> {translate('language2')}
                 </button>
               </li>
               <li>
                 <button onClick={() => changeLanguage('en')}>
-                  <img src={flagEn} alt="English flag" className="flag-icon" />
-                  {translate('language3')}
+                  <img src={flagEn} alt="English flag" className="flag-icon" /> {translate('language3')}
                 </button>
               </li>
             </ul>
