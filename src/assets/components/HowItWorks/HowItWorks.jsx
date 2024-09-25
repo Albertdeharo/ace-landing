bf183fd8af4bea95016e9a7d0348c6d7354bf9f4
albert.deharo <albert.deharo@basetis.com>
2024-09-25 12:51:31 +0200

    [fix] - howitworks style

--- src/assets/components/HowItWorks/HowItWorks.jsx
+++ src/assets/components/HowItWorks/HowItWorks.jsx
@@ -10,18 +10,18 @@ const steps = [
   {
     title: 'Paso 1: Limpia la Pala',
     description: 'Asegúrate de que la superficie de la pala esté limpia y seca antes de aplicar el spray.',
-    img: clean
+    img: clean,
   },
   {
     title: 'Paso 2: Aplica el Spray',
     description: 'Sostén el spray a unos 20 cm de la pala y aplica una capa uniforme sobre el grip.',
-    img: handGel
+    img: handGel,
   },
   {
     title: 'Paso 3: Deja Secar',
     description: 'Deja que el spray se seque durante unos minutos antes de comenzar a jugar.',
-    img: dry
-  }
+    img: dry,
+  },
 ];
 
 const HowItWorks = () => {
@@ -36,7 +36,7 @@ const HowItWorks = () => {
             title={step.title}
             description={step.description}
             img={step.img}
-            index={index} // Pasamos el índice para la clase
+            index={index}
           />
         ))}
       </div>
@@ -58,14 +58,18 @@ const AnimatedStep = ({ number, title, description, img, index }) => {
       animate={inView ? { opacity: 1, x: 0 } : { opacity: 0, x: index % 2 === 0 ? -100 : 100 }}
       transition={{ duration: 0.6, delay: index * 0.2 }}
     >
-      <div className="step-number">
-        <span>{number}</span>
-      </div>
-      <div className="step-content">
-        <div className={`neon__card step-${index + 1}`}>
-          <img src={img} alt={`Icono de ${title}`} className="step-icon" />
-          <h3>{title}</h3>
-          <p>{description}</p>
+      <div className="container">
+        <div className={`card ${index % 2 === 1 ? 'reverse-card' : 'normal-card'}`}>
+          <div className="box">
+            <div className="content">
+              <span className="heading">{number < 10 ? `${number}` : number}</span>
+              <img src={img} alt={`Icono de ${title}`} className="step-icon" />
+              <div className="text-content">
+                <h3>{title}</h3>
+                <p>{description}</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </motion.div>
