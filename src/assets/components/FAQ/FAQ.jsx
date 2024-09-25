dd1f81a078bdcfeae6bfc332adc1a7eb4a17fdfc
albert.deharo <albert.deharo@basetis.com>
2024-09-25 11:32:54 +0200

    [fix] - faq styles

--- src/assets/components/FAQ/FAQ.jsx
+++ src/assets/components/FAQ/FAQ.jsx
@@ -4,15 +4,30 @@ import './FAQ.css';
 const faqData = [
   {
     question: '¿Cuánto dura el efecto del spray?',
-    answer: 'El efecto del spray puede durar varias horas, dependiendo de las condiciones de uso.',
+    answer: (
+      <>
+        <h4>Título de la Respuesta</h4>
+        <p>El efecto del spray puede durar varias horas, dependiendo de las condiciones de uso.</p>
+      </>
+    ),
   },
   {
     question: '¿Es seguro para la piel?',
-    answer: 'Sí, el spray está formulado para ser seguro y no irritante para la piel.',
+    answer: (
+      <>
+        <h4>Seguridad del Producto</h4>
+        <p>Sí, el spray está formulado para ser seguro y no irritante para la piel.</p>
+      </>
+    ),
   },
   {
     question: '¿Puedo usarlo en cualquier tipo de pala?',
-    answer: 'Sí, es adecuado para la mayoría de las palas de padel y tenis.',
+    answer: (
+      <>
+        <h4>Compatibilidad</h4>
+        <p>Sí, es adecuado para la mayoría de las palas de padel y tenis.</p>
+      </>
+    ),
   },
 ];
 
@@ -28,9 +43,18 @@ const FAQ = () => {
       <h2>Preguntas Frecuentes</h2>
       <div className="faq-list">
         {faqData.map((item, index) => (
-          <div className="faq-item" key={index}>
-            <h3 onClick={() => toggleAnswer(index)}>{item.question}</h3>
-            {activeIndex === index && <p>{item.answer}</p>}
+          <div
+            onClick={() => toggleAnswer(index)}
+            className={`faq-item ${activeIndex === index ? 'active' : ''}`}
+            key={index}
+          >
+            <h3>
+              {item.question}
+              <span className="icon">{activeIndex === index ? '-' : '+'}</span>
+            </h3>
+            <div className="answer-content">
+              {activeIndex === index && item.answer}
+            </div>
           </div>
         ))}
       </div>
