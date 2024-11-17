0451c675a4969b402b9fd78f8e47f05095c0dc02
albert.deharo <albert.deharo@basetis.com>
2024-11-17 15:33:45 +0100

    some fixes

--- src/assets/pages/Contact.jsx
+++ src/assets/pages/Contact.jsx
@@ -44,14 +44,13 @@ const ContactForm = () => {
   return (
     <>
       <div className="contact-container">
-        <div className="contact-left">
-          <h2>Get In Touch</h2>
           <form onSubmit={handleSubmit} className="contact-form">
-            <div className="single-row">
+            <div className="form-box">
+              <label htmlFor="name">Nombre</label>
                 <input 
                   type="text" 
                   name="name"
-                  placeholder="First Name"
+                  placeholder="Enter your name"
                   className="form-in"
                   value={formState.name} 
                   onChange={handleChange} 
@@ -59,48 +58,38 @@ const ContactForm = () => {
                   autoComplete="off"
                 />
             </div>
-            <div className="multiple-row">
+            <div className="form-box">
+              <label htmlFor="email">Email</label>
               <input 
                 type="email" 
                 name="email" 
-                placeholder="Email"
+                placeholder="Enter your email"
                 className="form-in"
                 value={formState.email} 
                 onChange={handleChange} 
                 required
                 autoComplete="off"
               />
-              <textarea 
-                name="message"
-                className="form-in"
-                placeholder="Your Message"
-                value={formState.message} 
-                onChange={handleChange} 
-                required
-                autoComplete="off"
-              />
+            </div>
+              
+              <div className="">
+                <label htmlFor="message">Mensaje</label>
+                <textarea 
+                  name="message"
+                  className="form-in"
+                  placeholder="Your Message"
+                  value={formState.message} 
+                  onChange={handleChange} 
+                  required
+                  autoComplete="off"
+                  />
+              </div>
+
               <input type="submit" id="submit" />
               <label htmlFor="submit">
                 <i className="fas fa-paper-plane"></i>
               </label>
-            </div>
           </form>
-        </div>
-        <div className="contact-right">
-          <h2>Contact Informations</h2>
-          <div className="contact-info">
-            <p><i className="fas fa-phone-alt"></i> +977 9800000000 </p>
-            <p><i className="fas fa-envelope"></i> info@example.com </p>
-            <p><i className="fas fa-map-marker-alt"></i> Butwal, Nepal</p>
-          </div>
-
-          <div className="social-links">
-            <a href="#"><i className="fab fa-facebook"></i></a>
-            <a href="#"><i className="fab fa-instagram"></i></a>
-            <a href="#"><i className="fab fa-twitter"></i></a>
-            <a href="#"><i className="fab fa-youtube"></i></a>
-          </div>
-        </div>
       </div>
     </>
   );
