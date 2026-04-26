a6fa3697f8536ead78dd7499e626eab41bbf2aa0
Albertdeharo <albert.deharo.murcia@gmail.com>
2026-04-26 17:33:26 +0200

    maintenance mode

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,20 @@ import AppRoutes from "./Routes";
 
 function App() {
   return (
-    <>
-      <AppRoutes />
-    </>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        background: "#0f0f0f",
+        color: "white",
+      }}
+    >
+      <h1>🚧 Web en construcción</h1>
+      <p>Estamos mejorando ACE Pro Grip. Vuelve pronto.</p>
+    </div>
   );
 }
 
