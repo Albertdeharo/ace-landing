6deb0ac8214f85b7f1381ad033cddc9c2d1c3f27
Albert de Haro <albert.deharo@basetis.com>
2026-05-11 22:40:24 +0200

    refactor: update parallax component to use CSS classes for responsive image positioning and replace background asset

--- src/assets/components/Parallax/Parallax.jsx
+++ src/assets/components/Parallax/Parallax.jsx
@@ -1,18 +1,11 @@
 import { Parallax } from 'react-parallax';
 import './Parallax.css';
-
-import parallaxImg from './../../images/parallax.jpg';
+import parallaxImg from './../../images/hero-parallax.jpg';
 
 const ParallaxSection = () => {
   return (
-    <Parallax 
-      bgImage={parallaxImg} 
-      strength={300}
-      bgImageStyle={{ 
-        objectFit: 'cover', 
-        objectPosition: 'center center' 
-      }}
-    >
+    <Parallax bgImage={parallaxImg} strength={300} >
+      {/* Crucial: We use a class, not inline styles, to make it responsive */}
       <div className="parallax-content"></div>
     </Parallax>
   );
